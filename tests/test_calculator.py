1085820a0226a30f19380532c215014fc5b7d76a
vvh24 <valeriahcrespo@hotmail.com>
2024-10-08 19:51:59 -0400

    resolved merge conflict in test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,31 +4,22 @@ faker = Faker()
 '''My Calculator Test'''
 from calculator import Calculator
 
-<<<<<<< HEAD
 def test_operations(generate_test_data):
     for a, b, operation in generate_test_data:
         if operation == 'add':
             result = a + b
-            assert add(a, b) == result
+            assert Calculator.add(a, b) == result
         elif operation == 'subtract':
             result = a - b
-            assert subtract(a, b) == result
+            assert Calculator.subtract(a, b) == result
+        elif operation == 'multiply':
+            result = a * b
+            assert Calculator.multiply(a, b) == result
+        elif operation == 'divide':
+            if b != 0:  # Handling division by zero
+                result = a / b
+                assert Calculator.divide(a, b) == result
+            else:
+                assert b != 0, "Division by zero is not allowed."
             
-=======
-def test_addition():
-    '''Test that the addition function works.'''
-    assert Calculator.add(3, 5) == 8
 
-def test_subtraction():
-    '''Test that the subtraction function works.'''
-    assert Calculator.subtract(10, 4) == 6
-
-def test_divide():
-    '''Test that the division function works.'''
-    assert Calculator.divide(12, 4) == 3
-
-def test_multiply():
-    '''Test that the multiplication function works.'''
-    assert Calculator.multiply(3, 7) == 21
-    
->>>>>>> part3
